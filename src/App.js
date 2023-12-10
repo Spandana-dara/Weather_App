25ce6733e74ec908a5f41d96cab3b0dcfd684577
Spandana <spandana1286@gmail.com>
2023-12-10 23:40:54 +0530

    added assets

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
+import WeatherApp from "./Components/WeatherApp/WeatherApp";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <WeatherApp/>
+  </div>;
 }
 
 export default App;
---
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './WeatherApp.css';
+import search_icon from '../Assets/search.png';
+import clear_icon from '../Assets/clear.png';
+import cloud_icon from '../Assets/cloud.png';
+import drizzle_icon from '../Assets/drizzle.png';
+import rain_icon from '../Assets/rain.png';
+import snow_icon from '../Assets/snow.png';
+import wind_icon from '../Assets/wind.png';
+import humidity_icon from '../Assets/humidity.png';
+
+function WeatherApp() {
+  return (
+    <div className="container">
+        <div className="top-bar">
+            <input type="text" className="cityInput" placeholder='Search'/>
+        </div>
+    </div>
+  )
+}
+
+export default WeatherApp
