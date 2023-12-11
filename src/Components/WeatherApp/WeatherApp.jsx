991bae3a9e8ecebfa678d3cc3c4ec0f68a047869
Spandana <spandana1286@gmail.com>
2023-12-11 12:13:16 +0530

    final changes

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -10,28 +10,49 @@ import wind_icon from '../Assets/wind.png';
 import humidity_icon from '../Assets/humidity.png';
 
 function WeatherApp() {
-  let api_key="0f0e4173eba7505369617f8f8af2cdb0";
+  let api_key = "0f0e4173eba7505369617f8f8af2cdb0";
   const [wicon, setWicon] = useState(cloud_icon)
-  const search = async () =>{
-      const element = document.getElementsByClassName("cityInput");
-      if(element[0].value===""){
-        return 0;
-      }
-      let url = `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&units=Metric&appid=${api_key}`;
+  const search = async () => {
+    const element = document.getElementsByClassName("cityInput");
+    if (element[0].value === "") {
+      return 0;
+    }
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&units=Metric&appid=${api_key}`;
 
-      let response = await fetch(url);
-      let data = await response.json();
-      const humidity = document.getElementsByClassName("humidity-percent");
-      const wind = document.getElementsByClassName("wind-speed");
-      const temperature = document.getElementsByClassName("weather-temp");
-      const location = document.getElementsByClassName("weather-location");
-      humidity[0].innerHTML = data.main.humidity+ " %";
-      wind[0].innerHTML = data.wind.speed + " km/hr";
-      temperature[0].innerHTML =  data.main.temp + "&deg;c" ;
-      location[0].innerHTML = data.name;
-      if(data.weather[0].icon==="01d" || data.weather[0].icon==="o1n"){
-        setWicon(clear_icon);
-      }
+    let response = await fetch(url);
+    let data = await response.json();
+    const humidity = document.getElementsByClassName("humidity-percent");
+    const wind = document.getElementsByClassName("wind-speed");
+    const temperature = document.getElementsByClassName("weather-temp");
+    const location = document.getElementsByClassName("weather-location");
+    humidity[0].innerHTML = data.main.humidity + " %";
+    wind[0].innerHTML = Math.floor(data.wind.speed) + " km/hr";
+    temperature[0].innerHTML = Math.floor(data.main.temp) + "&deg;c";
+    location[0].innerHTML = data.name;
+    if (data.weather[0].icon === "01d" || data.weather[0].icon === "01n") {
+      setWicon(clear_icon);
+    }
+    else if (data.weather[0].icon === "02d" || data.weather[0].icon === "02n") {
+      setWicon(cloud_icon);
+    }
+    else if (data.weather[0].icon === "03d" || data.weather[0].icon === "03n") {
+      setWicon(drizzle_icon);
+    }
+    else if (data.weather[0].icon === "04d" || data.weather[0].icon === "04n") {
+      setWicon(drizzle_icon);
+    }
+    else if (data.weather[0].icon === "09d" || data.weather[0].icon === "09n") {
+      setWicon(rain_icon);
+    }
+    else if (data.weather[0].icon === "10d" || data.weather[0].icon === "10n") {
+      setWicon(rain_icon);
+    }
+    else if (data.weather[0].icon === "13d" || data.weather[0].icon === "13n") {
+      setWicon(snow_icon);
+    }
+    else {
+      setWicon(clear_icon)
+    }
 
   }
   return (
