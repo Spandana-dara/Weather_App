31bc12fcea22a4b820687e45163baa0ee1662fc9
Spandana <spandana1286@gmail.com>
2023-12-11 12:05:14 +0530

    icon logic

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './WeatherApp.css';
 import search_icon from '../Assets/search.png';
 import clear_icon from '../Assets/clear.png';
@@ -10,16 +10,40 @@ import wind_icon from '../Assets/wind.png';
 import humidity_icon from '../Assets/humidity.png';
 
 function WeatherApp() {
+  let api_key="0f0e4173eba7505369617f8f8af2cdb0";
+  const [wicon, setWicon] = useState(cloud_icon)
+  const search = async () =>{
+      const element = document.getElementsByClassName("cityInput");
+      if(element[0].value===""){
+        return 0;
+      }
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&units=Metric&appid=${api_key}`;
+
+      let response = await fetch(url);
+      let data = await response.json();
+      const humidity = document.getElementsByClassName("humidity-percent");
+      const wind = document.getElementsByClassName("wind-speed");
+      const temperature = document.getElementsByClassName("weather-temp");
+      const location = document.getElementsByClassName("weather-location");
+      humidity[0].innerHTML = data.main.humidity+ " %";
+      wind[0].innerHTML = data.wind.speed + " km/hr";
+      temperature[0].innerHTML =  data.main.temp + "&deg;c" ;
+      location[0].innerHTML = data.name;
+      if(data.weather[0].icon==="01d" || data.weather[0].icon==="o1n"){
+        setWicon(clear_icon);
+      }
+
+  }
   return (
     <div className="container">
       <div className="top-bar">
         <input type="text" className="cityInput" placeholder='Search' />
-        <div className='search-icon'>
+        <div className='search-icon' onClick={search}>
           <img src={search_icon}></img>
         </div>
       </div>
       <div className="weather-image">
-        <img src={cloud_icon}></img>
+        <img src={wicon}></img>
       </div>
       <div className="weather-temp">24c</div>
       <div className="weather-location">London</div>
@@ -34,7 +58,7 @@ function WeatherApp() {
         <div className="element">
           <img src={wind_icon} alt="" className="icon" />
           <div className="data">
-            <div className="humidity-percent">18km/hr</div>
+            <div className="wind-speed">18km/hr</div>
             <div className="text">Wind Speed</div>
           </div>
         </div>
